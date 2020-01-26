c082da150c29917405510a7dd759c80dfb97fd28
LeonidKursenko <kursenkoleonid@yandex.ua>
2020-01-26 16:22:07 +0200

    Add project files.

---
+++ Lab2/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using BreadthFirstSearch.Task;
+
+namespace BreadthFirstSearch
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public CheckerField StartField { set; get; }
+        public CheckerField FinalField { set; get; }
+        private Bfs _bfs;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _bfs = new Bfs();
+            _bfs.Run();
+            StartField = _bfs.StartField;
+            CheckerGrid.DataContext = StartField;
+            FinalField = _bfs.FinalField;
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            StartField.InitPath();
+            _bfs.Path = StartField.Path;
+            _bfs.InitPath();
+            _bfs.Step();
+            StartField.InitStartFieldCells();
+            MessageBox.Show("Шлях був знайдений. Довжина=" + _bfs.Path.Count);
+        }
+        
+        private void Pause()
+        {
+            CheckerGrid.Dispatcher.Invoke(DispatcherPriority.Background,
+                new Action(delegate() { CheckerGrid.UpdateLayout(); }));
+            Thread.Sleep(300);
+        }
+
+        private void ShowButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+            foreach (var move in _bfs.Path)
+            {
+                _bfs.PathHistory.Add(move.ToString());
+                move.Transfer();
+
+                CheckerGrid.DataContext = null;
+                CheckerGrid.DataContext = StartField;
+                ResultRichTextBox.Document.Blocks.Clear();
+                ResultRichTextBox.Document.Blocks.Add(new Paragraph(new Run(_bfs.GetPath)));
+                Pause();
+            }
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            StartField.InitStartFieldCells();
+            _bfs.StartField = StartField;
+            _bfs.PathHistory.Clear();
+            CheckerGrid.DataContext = null;
+            CheckerGrid.DataContext = StartField;
+            ResultRichTextBox.Document.Blocks.Clear();
+        }
+    }
+}
---
+++ Lab2/Task/Bfs.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace BreadthFirstSearch.Task
+{
+    public class Bfs
+    {
+        public CheckerField StartField { set; get; }
+        public CheckerField FinalField { set; get; }
+        public List<string> PathHistory { set; get; }
+        public List<Move> Path { set; get; }
+        public List<List<Move>> AllPathes { set; get; }
+
+        public string GetPath
+        {
+            get { return string.Join(",\n", PathHistory); }
+        }
+
+        public void Run()
+        {
+            StartField = new CheckerField();
+            StartField.InitStartFieldCells();
+
+            FinalField = new CheckerField();
+            FinalField.InitEndFieldCells();
+
+            Path = new List<Move>();
+            PathHistory = new List<string>();
+            AllPathes = new List<List<Move>>();
+        }
+
+        private int counter = 0;
+        public void Step()
+        {
+            //if (StartField.Equals(FinalField)) return;
+            Cell freeCell = StartField.Cells.First(x => x.Content == null);
+            List<Move> availableMoves = StartField.Moves.Where(x => x.HasCell(freeCell)).ToList();
+            AllPathes = availableMoves.Select(x => new List<Move> {x}).ToList();
+
+            while (AllPathes.Count > 0)
+            {
+                Path = AllPathes[0];
+                AllPathes.RemoveAt(0);
+                InitPath();
+                if (StartField.Equals(FinalField)) return;
+                freeCell = StartField.Cells.First(x => x.Content == null);
+                availableMoves = StartField.Moves.Where(x => x.HasCell(freeCell)).ToList();
+                var moves = new List<Move>();
+                foreach (var move in availableMoves)
+                {
+                    if (move.Checker.Moves.Exists(x => x.Equals(move))) continue;
+                    moves.Add(move);
+                }
+                availableMoves = moves;
+                AllPathes.AddRange(availableMoves.Select(x =>
+                {
+                    var path = Path.ToList();
+                    path.Add(x);
+                    return path;
+                }).ToList());
+            }
+
+            Path.Clear();
+            AllPathes.Clear();
+        }
+
+
+        public void Step0()
+        {
+            if (StartField.Equals(FinalField)) return;
+            Cell freeCell = StartField.Cells.First(x => x.Content == null);
+            List<Move> availableMoves = StartField.Moves.Where(x => x.HasCell(freeCell)).ToList();
+            foreach (var move in availableMoves)
+            {
+                if(move.Checker.Moves.Exists(x => x.Equals(move))) continue;
+                Path.Add(move);
+                move.Transfer();
+
+                if (StartField.Equals(FinalField)) return;
+                #region Rollback
+                if (Path.Count > 15)
+                {
+                    Path.RemoveAt(Path.Count - 1);
+                    move.RollBack();
+                    return;
+                }
+                #endregion
+                Step0();
+                if (StartField.Equals(FinalField)) return;
+                Path.RemoveAt(Path.Count - 1);
+                move.RollBack();
+            }
+        }
+
+        public void InitPath()
+        {
+            //StartField = new CheckerField();
+            StartField.InitStartFieldCells();
+            //StartField.InitPath();
+            //Path = StartField.Path;
+            foreach (var move in Path)
+            {
+                move.Transfer();
+            }
+        }
+    }
+}
